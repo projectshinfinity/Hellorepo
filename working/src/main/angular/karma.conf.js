4d6478ac4eac0dea30b66c5a7ce396bb125c4a66
EC2 Default User <ec2-user@ip-172-31-43-142.ap-south-1.compute.internal>
2025-01-09 10:53:53 +0000

    mvn build

---
+++ working/src/main/angular/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // You can configure Jasmine here if needed
+      },
+      clearContext: false // Leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // Removes the duplicated traces
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
